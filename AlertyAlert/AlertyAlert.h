a19601d3634c0c0aa31e1ffc67255534aa2c760c
Peter Prokop <pitrowech@gmail.com>
2017-01-23 12:00:32 +0100

    First magical commit

---
+++ AlertyAlert/AlertyAlert.h
@@ -0,0 +1,19 @@
+//
+//  AlertyAlert.h
+//  AlertyAlert
+//
+//  Created by Peter Prokop on 23/01/2017.
+//  Copyright © 2017 Prokop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlertyAlert.
+FOUNDATION_EXPORT double AlertyAlertVersionNumber;
+
+//! Project version string for AlertyAlert.
+FOUNDATION_EXPORT const unsigned char AlertyAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertyAlert/PublicHeader.h>
+
+
